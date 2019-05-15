57dd9685fafc3b515d7ae98fc7c88be6fd69737f
Edvyn Tanu <edvyntanu@gmail.com>
2019-05-15 12:17:39 +0700

    Initial Commit

---
+++ Learning OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learning OOP
+//
+//  Created by Silamitra Edvyn Tanu on 15/05/19.
+//  Copyright © 2019 Silamitra Edvyn Tanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
